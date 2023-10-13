810fd9c7e71b4d538f631fac41292dad3f892c7d
Cole Chiga <colejchiga@gmail.com>
2023-10-13 16:20:43 -0600

    Set up and initial commit

---
+++ src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
@@ -0,0 +1,18 @@
+package com.skilldistillery.foodtruck.app;
+
+import com.skilldistillery.foodtruck.entities.*;
+
+public class FoodTruckApp {
+
+	private FoodTruck[] fleet = new FoodTruck[5];
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FoodTruckApp fta = new FoodTruckApp();
+		fta.run();
+	}
+
+	public void run(){
+		
+	}
+}
